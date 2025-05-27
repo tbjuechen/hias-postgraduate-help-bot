239cc8a6c200ea0ccdf293233d7b47ffc2bd0b6c
tbjuechen <1324374092@qq.com>
2025-05-27 22:20:48 +0800

    fix: plugin load

--- bot.py
+++ bot.py
@@ -2,12 +2,14 @@ from nonebot import get_driver, init
 from nonebot.adapters.onebot.v11 import Adapter
 import os
 from dotenv import load_dotenv
+import nonebot
 
 load_dotenv()  # 如果用 .env 文件配置环境变量
 
 init()
 driver = get_driver()
 driver.register_adapter(Adapter)
+nonebot.load_plugins("plugins")
 
 if __name__ == "__main__":
     driver.run()
